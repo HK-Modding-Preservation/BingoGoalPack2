9fa3cfb9b313915428c559f25ab2128c6a133da0
TheMathGeek_314 <16294237+TheMathGeek314@users.noreply.github.com>
2024-10-29 22:00:54 -0500

    Add project files.

---
+++ BingoGoalPack2.cs
@@ -0,0 +1,33 @@
+﻿using Modding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using BingoSync.CustomGoals;
+
+
+namespace BingoGoalPack2 {
+    public class BingoGoalPack2: Mod {
+        new public string GetName() => "BingoGoalPack2";
+        public override string GetVersion() => "1.0.0.0";
+        public override int LoadPriority() => 8;
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects) {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            Dictionary<string, BingoGoal> myGoals = processEmbeddedJson(assembly, "Goals");
+            GameMode mode = new("GoalPack2", myGoals);
+            //BingoSync.Goals.AddGameMode(mode);
+            BingoSync.Goals.RegisterGoalsForCustom("Goal Pack 2", myGoals);
+        }
+
+        private Dictionary<string, BingoGoal> processEmbeddedJson(Assembly assembly, string jsonName) {
+            string resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith("Squares." + jsonName + ".json"));
+            Stream stream = assembly.GetManifestResourceStream(resourceName);
+            return BingoSync.Goals.ProcessGoalsStream(stream);
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# BingoGoalPack2
+
+A mod that expands on the capabilities of BingoSync.
+
+The aim of this goal pack is to enable more granular specifications within custom bingo challenges, including auto-marking and board generation.
+
+This pack includes:
+- All movement, spells, and abilities
+- Every charm
+- Most overworld bosses and dreamers
+- Progression-enabling items like Brand and Tram Pass
